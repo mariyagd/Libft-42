d35ee79c1c5698b194e0cb34d0417178a976445c
mariyagd <mariya.g.dancheva@icloud.com>
2022-08-30 12:44:57 +0200

    norminette ok, tester ok

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,36 +6,33 @@
 /*   By: mdanchev <mdanchev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/19 23:15:52 by mdanchev          #+#    #+#             */
-/*   Updated: 2022/08/26 11:12:30 by mdanchev         ###   ########.fr       */
+/*   Updated: 2022/08/30 12:44:10 by mdanchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	i;
-	size_t	k;
-	size_t	count;
+	size_t	len_needle;
+	size_t	len_haystack;
 
-	k = 0;
-	i = 0;
-	count = 0;
-	if (needle[k] == '\0')
+	len_needle = ft_strlen(needle);
+	len_haystack = ft_strlen(haystack);
+	if (needle[0] == '\0')
 		return ((char *)(haystack));
-	while (i < len && haystack[i])
+	if (len_needle > len_haystack || len < len_needle)
+		return (NULL);
+	if (len > len_haystack)
+		len = len_haystack;
+	while (len >= len_needle && haystack)
 	{
-		while (haystack[i] == needle[k] && i < len)
+		if (ft_memcmp(haystack, needle, len_needle) == 0)
+			return ((char *)(haystack));
+		else
 		{
-			i++;
-			k++;
-			count++;
-			if (count == ft_strlen(needle))
-				return ((char *)(haystack +(i - count)));
+			haystack++;
+			len--;
 		}
-		if (haystack[i] != needle[0])
-			i++;
-		k = 0;
-		count = 0;
 	}
 	return (NULL);
 }
