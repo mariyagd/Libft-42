c0730063dd1127665ed2b72e4b59e14a30a8458e
mariyagd <mariya.g.dancheva@icloud.com>
2022-10-15 20:22:37 +0200

    norminette ok, tester ok
    les conditions ont été décrites plus explicitement afin d'avoir une meilleure compréhension

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: mdanchev <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 14:23:43 by mdanchev          #+#    #+#             */
-/*   Updated: 2022/10/13 16:25:12 by mdanchev         ###   ########.fr       */
+/*   Updated: 2022/10/15 20:20:22 by mdanchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
@@ -22,9 +22,9 @@ char	*ft_strtrim(char const *s1, char const *set)
 	front = 0;
 	i = 0;
 	end = (unsigned int)(ft_strlen(s1));
-	while (s1[front] && ft_strchr(set, *(s1 + front)))
+	while (s1[front] != '\0' && ft_strchr(set, *(s1 + front)) != NULL)
 			front++;
-	while (front < end && ft_strrchr(set, *(s1 + end - 1)))
+	while (front < end && ft_strrchr(set, *(s1 + end - 1)) != NULL)
 			end--;
 	strtrim = (char *)malloc((end - front + 1) * sizeof(char));
 	if (!strtrim)
