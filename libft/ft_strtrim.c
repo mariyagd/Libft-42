cec35884efa3f6f0b7f9a4c942e8cb0289c8cb1a
mariyagd <109855801+mariyagd@users.noreply.github.com>
2022-10-20 15:34:06 +0200

    norminette ok, tester ok

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -9,7 +9,6 @@
 /*   Updated: 2022/10/15 20:20:22 by mdanchev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdlib.h>
 #include "libft.h"
 
 char	*ft_strtrim(char const *s1, char const *set)
